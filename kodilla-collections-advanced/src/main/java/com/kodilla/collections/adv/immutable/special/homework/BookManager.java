f023ba2fc1e13a96dad9dcfddb92256bec5cb6fd
Katarzyna Szymkowiak <katarzyna.chmielewska@hotmail.com>
2021-02-01 16:04:24 +0100

    5.4 ksiegozbior

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -0,0 +1,8 @@
+package com.kodilla.collections.adv.immutable.special.homework;
+
+public class BookManager {
+
+    public static Book createBook(String title, String author) {
+        return null;
+    }
+}
