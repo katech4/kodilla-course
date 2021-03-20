1803458c701814143327c2d40ed022de0c0976dc
Katarzyna Szymkowiak <katarzyna.chmielewska@hotmail.com>
2021-03-20 11:31:02 +0100

    6.2. Typ optional

--- kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -1,18 +1,16 @@
 package com.kodilla.optional.homework;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
 public class Application {
     public static void main(String[] args) {
-        List<Student> studentList = new ArrayList<>();
-        studentList.add(new Student("Kasia", null));
-        studentList.add(new Student("Maciej", null));
-        studentList.add(new Student("Michal",null));
 
-        /*Optional<Student> optionalStudent = Optional.ofNullable(/*???nie wiem która zmienna)*/;/*
+        Student Kasia = new Student("Kasia", null);
 
-        optionalStudent.ifPresent(u -> System.out.println(u.getName())); System.out.println(u.getTeacher());
-        /*tutaj też nie wiem do końca jakie są opcje wyświtlania....prośba o podpowiedź*/
+        Optional<Teacher> optionalTeacher = Optional.ofNullable(Kasia.getTeacher());
+
+        Teacher teacher = optionalTeacher.orElse(new Teacher("<undefined>"));
+        System.out.println(teacher.getName());
+
     }
 }
