32e25f2a6a185add84a1121c851065f66ec88dda
Katarzyna Szymkowiak <katarzyna.chmielewska@hotmail.com>
2021-03-29 22:28:28 +0200

    13.5 Selectory CSS

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -1,11 +1,16 @@
-/*package com.kodilla.selenium.allegro;
+package com.kodilla.selenium.allegro;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class AllegroTestingApp {
+
+    private static WebElement categoryCombo;
 
     public static void main(String[] args) {
 
@@ -13,15 +18,15 @@ public class AllegroTestingApp {
         WebDriver driver = new ChromeDriver();                    // [2]
         driver.get("https://www.allegro.pl/");                     // [3]
 
-        WebElement inputField = driver.findElement(By.xpath("//+[@id=\"main_wrapper\"]/div/span/span/select[3]")); // [4]
+        WebElement inputField = driver.findElement(By.cssSelector("#opbox-listing")); // [4]
+        List<WebElement> element = driver.findElements(By.className("#opbox-listing"));
+        inputField = driver.findElement(By.xpath("//html/body/div/div/form/fieldset[2]/input"));
         inputField.sendKeys("Mavic mini");
-        //inputField = driver.findElement(By.xpath("//html/body/div/div/form/fieldset[2]/input"));
-        //inputField.sendKeys("Haslo");
 
-        //Select category = new Select(categoryCombo);
-        //category = new Select(categoryCombo);
-        //category.selectByIndex(3);
+        Select category = new Select(categoryCombo);
+        category = new Select(categoryCombo);
+        category.selectByIndex(3);
 
 
     }
-}*/
+}
