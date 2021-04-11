a3f5501ba43bb9ba28584cddd2e2c270f349fb99
Katarzyna Szymkowiak <katarzyna.chmielewska@hotmail.com>
2021-04-11 19:55:05 +0200

    5.4 Niemutowalność

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -1,10 +1,20 @@
 package com.kodilla.collections.adv.immutable.special.homework;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookApplication {
     public static void main(String[] args) {
+
+
       Book book = BookManager.createBook("W pustyni i w puszczy", "H.Sienkiewicz");
       Book book1 = BookManager.createBook("Lalka", "B.Prus");
-      Book book2 = BookManager.createBook("Pan Tadeusz", "A.Mickiewicz");
+      Book book2 = BookManager.createBook("Lalka", "B.Prus");
+
+      List<Book> books = new ArrayList<>();
+      books.add(book1);
+      books.add(book2);
+
 
         System.out.println(book == book1);
         System.out.println(book.equals(book1));
@@ -14,7 +24,5 @@ public class BookApplication {
         System.out.println("--------------------");
         System.out.println(book1 == book2);
         System.out.println(book1.equals(book2));
-
-
     }
 }
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -1,21 +1,9 @@
 package com.kodilla.collections.adv.immutable.special.homework;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BookManager {
 
     public static Book createBook(String title, String author) {
 
-        List<Book> books = new ArrayList<>();
-        books.add(new Book("W pustyni i w puszczy", "H. Sienkiewicz"));
-        books.add(new Book("Chata", "Young"));
-
-        for (Book book : books) {
-            if (book.equals(books))
-            System.out.println(book);
-            else System.out.println(books);
-        }
         return new Book(title, author);
     }
 }
